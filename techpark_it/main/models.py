3169ebb9a9de478e6e6d4746788b029e4e872425
musfiq <musfiqurrahmansayed@gmail.com>
2025-03-24 22:57:31 +0600

    Create basic models for Teachers, Students, Courses and Course Categories and added them to admin

--- techpark_it/main/models.py
+++ techpark_it/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+    
+    
+class CourseCategory(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Course Categories"
+      
+class Course(models.Model):
+    category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+    interested_categories = models.TextField()
